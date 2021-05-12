ee835fcf49c96f5560bfb4f5ee4da45203883b4f
Jan9669 <jan96hoping@gmail.com.com>
2021-05-12 11:00:54 +0200

    include map

---
+++ uebung4.js
@@ -0,0 +1,4 @@
+var mymap = L.map('mapid').setView([51.9692307002609,7.595822811126709], 6);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
+
+//7.595822811126709,51.9692307002609
