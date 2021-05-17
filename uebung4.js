84083c00382bed7c58d39fcadaad961f267c5906
Jan9669 <jan96hoping@gmail.com.com>
2021-05-17 02:04:20 +0200

    add draw function

--- uebung4.js
+++ uebung4.js
@@ -1,4 +1,34 @@
-var mymap = L.map('mapid').setView([51.9692307002609,7.595822811126709], 6);
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(mymap);
+// Create the map
+var map = L.map('map',{drawControl: true}).setView([51.9692307002609,7.595822811126709], 6);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-//7.595822811126709,51.9692307002609
+// Initialise the FeatureGroup to store editable layers
+var editableLayers = new L.FeatureGroup();
+map.addLayer(editableLayers);
+//draw polygon
+var drawPluginOptions = {
+position: 'topleft',
+draw: {
+  rectangle: true,
+},
+edit: {
+  featureGroup: editableLayers, //REQUIRED!!
+  remove: false,
+  
+}
+};
+
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+map.on('draw:created', function(e) {
+
+  layer = e.layer;
+editableLayers.addLayer(layer);
+});
+
+//German Route
+L.geoJSON(geojsonFeature).addTo(map);
+
+    
+
